cb34a5e56af0ac0929f34ec1e8de1a12fdd3c28e
nicolastluizetti <96789314+nicolastluizetti@users.noreply.github.com>
2024-01-28 20:36:28 -0300

    Add files via upload

---
+++ GestãodeConvidados-1/src/main/java/com/ntl/festa/model/ConvidadosRepository.java
@@ -0,0 +1,7 @@
+package com.ntl.festa.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConvidadosRepository extends JpaRepository<Convidados, Long>{
+
+}
---
+++ GestãodeConvidados-1/src/test/java/com/ntl/festa/GestaodeConvidados1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntl.festa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaodeConvidados1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
