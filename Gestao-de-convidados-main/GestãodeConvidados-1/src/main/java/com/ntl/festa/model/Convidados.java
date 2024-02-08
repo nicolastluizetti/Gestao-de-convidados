f57cb605eeca1136addc8ac67726ae6a8c8a2437
nicolastluizetti <96789314+nicolastluizetti@users.noreply.github.com>
2024-02-08 12:52:30 -0300

    Delete Gestao-de-convidados-main directory

--- Gestao-de-convidados-main/GestãodeConvidados-1/src/main/java/com/ntl/festa/model/Convidados.java
+++
@@ -1,7 +0,0 @@
-package com.ntl.festa.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Convidados extends JpaRepository<Convidado, Long>{
-
-}
--- Gestao-de-convidados-main/GestãodeConvidados-1/target/classes/import.sql
+++
@@ -1,3 +0,0 @@
-insert into Convidado (id, nome, quantidade_acompanhantes) values(1, 'Pedro', 2);
-insert into Convidado (id, nome, quantidade_acompanhantes) values(2, 'Maria', 3);
-insert into Convidado (id, nome, quantidade_acompanhantes) values(3, 'Ricardo', 1);
